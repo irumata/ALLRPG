5e99007e2d766da63001e7a5b85ffeacbf418c28
Leonid Tsarev <leo@bastilia.ru>
2015-11-07 22:26:42 +0300

    First version of improtproject

---
+++ site1/public_html/joinrpg/importproject.php
@@ -0,0 +1,34 @@
+﻿<?php
+
+require_once("../db.inc");
+require_once("../classes_objects_allrpg.php");
+
+require_once '../appcode/data/roles_main.php';
+require_once 'joinrpg_common.php';
+
+start_mysql();
+# Установление соединения с MySQL-сервером
+
+$id = intval($_GET['id']);
+$key = $_GET['key'];
+
+check_key($id, $key);
+
+$user_table = array();
+
+$roles = load_all_roles ($id, /*team:*/ FALSE);
+
+foreach ($roles as $role)
+{
+  if (!array_key_exists($role['sid'], $user_table))
+  {
+  
+    $user_table[$role['sid']] = whitelist($role, 'user');
+  }
+}
+
+$result['project_name'] = "project$id";
+$result['users'] = array_values($user_table);
+
+echo json_encode($result);
+?>
