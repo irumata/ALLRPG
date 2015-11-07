111bf91a9f6475ccb879ad8f8f8f2315158ade0c
Leonid Tsarev <leo@bastilia.ru>
2015-11-07 22:26:31 +0300

    Move common functions from joinrpg\profile

---
+++ site1/public_html/joinrpg/joinrpg_common.php
@@ -0,0 +1,40 @@
+﻿<?php
+require_once("../db.inc");
+require_once("../classes_objects_allrpg.php");
+require_once "../appcode/data/common.php";
+
+function check_key($payload, $key)
+{
+  global $joinrpg_connector_secret_key;
+  $key_to_check = sha1("$payload$joinrpg_connector_secret_key");
+  if ($key != $key_to_check)
+  {
+    echo 'ERROR_WRONG_KEY';
+    die();
+  }
+}
+
+function whitelist($row, $type)
+{
+  $whitelist = get_whitelist($type);
+  $row_whitelisted = array();
+  foreach ($row as $key => $value)
+  {
+    if ($key && in_array($key, $whitelist))
+    {
+      $row_whitelisted[$key] = $value;
+    }
+  }
+  return $row_whitelisted;
+}
+
+function get_whitelist($type)
+{
+ switch ($type)
+  {
+      case 'user': return array("sid", "fio", "nick", "gender", "medic", "em", "em2", "phone2", "icq", "skype", "jabber", "vkontakte", "tweeter", "livejournal", "googleplus", "facebook", "photo", "login",   "birth", "city", "sickness", "additional", "prefer", "prefer2", "prefer3", "prefer4", "specializ", "ingroup", "hidesome", "date");
+      default: return array();
+  }
+}
+ 
+?>
--- site1/public_html/joinrpg/profile.php
+++ site1/public_html/joinrpg/profile.php
@@ -3,6 +3,8 @@ require_once("../db.inc");
 require_once("../classes_objects_allrpg.php");
 require_once "..//appcode/data/common.php";
 
+require_once 'joinrpg_common.php';
+
 start_mysql();
 # Установление соединения с MySQL-сервером
 
@@ -11,12 +13,7 @@ $content='';
 $email = $_GET['email'];
 $key = $_GET['key'];
 
-$key_to_check = sha1("$email$joinrpg_connector_secret_key");
-if ($key != $key_to_check)
-{
-  echo 'ERROR_WRONG_KEY';
-  die();
-}
+check_key($email, $key);
 
 $email_escaped = mysql_real_escape_string($email);
 
@@ -28,16 +25,5 @@ if (!intval($row['sid']))
   die();
 }
 
-$whitelist = array("sid", "fio", "nick", "gender", "medic", "em", "em2", "phone2", "icq", "skype", "jabber", "vkontakte", "tweeter", "livejournal", "googleplus", "facebook", "photo", "login",   "birth", "city", "sickness", "additional", "prefer", "prefer2", "prefer3", "prefer4", "specializ", "ingroup", "hidesome", "date");
-
-$row_whitelisted = array();
-foreach ($row as $key => $value)
-{
-  if ($key && in_array($key, $whitelist))
-  {
-    $row_whitelisted[$key] = $value;
-  }
-}
-
-echo json_encode($row_whitelisted);
+echo json_encode(whitelist($row, 'user'));
 ?>
